42ae1cd8c04cb71507c475b2995f408e7f4de960
Maaz Abdul Jabbar <maazmaster1@gmail.com>
2019-10-20 06:30:54 +0500

    first commit

---
+++ App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { StyleSheet, Text, View, ScrollView, FlatList } from 'react-native';
+import { Provider } from 'react-redux'
+import Routes from './Routes'
+import store from './redux/index';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <Provider store={store}>
+        <Routes />
+      </Provider>
+    );
+  }
+}
